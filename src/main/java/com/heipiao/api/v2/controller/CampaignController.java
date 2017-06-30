0a447ebaad75f68f996d478bd77799bddde7b693
duzh <duzh@heipiaola.com>
2017-06-30 12:08:02 +0800

    添加详情回顾mapper

--- src/main/java/com/heipiao/api/v2/controller/CampaignController.java
+++ src/main/java/com/heipiao/api/v2/controller/CampaignController.java
@@ -23,14 +23,12 @@ import com.heipiao.api.v2.domain.CampaignActor;
 import com.heipiao.api.v2.exception.BadRequestException;
 import com.heipiao.api.v2.service.ActivityArticleService;
 import com.heipiao.api.v2.service.CampaignService;
-import com.heipiao.api.v2.service.UserService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-
 /**
  * 有关活动的功能：<br>
  * 创建活动、报名、抽签、活动列表、活动管理等
@@ -50,28 +48,31 @@ public class CampaignController {
 
 	@Resource
 	private ActivityArticleService activityArticleService;
-	
-	@Resource
-	private UserService userService;
 
 	@ApiOperation(value = "获取活动信息", response = Campaign.class)
+	@ApiImplicitParam(paramType = "path", name = "id", value = "营销活动id", dataType = "int", required = true, example = "1")
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public Campaign getCampaign(
-			@ApiParam(value = "活动id", required = true) @PathVariable("id") Integer id) {
+			@PathVariable(value = "id", required = true) int id) {
 		logger.debug("id={}", id);
-
+		
 		Campaign campaign = campaignService.getCampaign(id);
 		return campaign;
 	}
 
 	// 跟这里类似的返回类型可以思考一下，传Map感觉不合适，但除了pojo，还有其他选择吗？
 	@ApiOperation(value = "获取活动参与人信息", response = Map.class)
+	@ApiImplicitParams({
+		@ApiImplicitParam(paramType = "path", name = "cid", value = "活动id", dataType = "int", required = true)
+		, @ApiImplicitParam(paramType = "path", name = "uid", value = "用户id", dataType = "int", required = true)
+	})
 	@RequestMapping(value = "actor/{cid}/{uid}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public Map<String, Object> getCampaignActor(
-			@ApiParam(value = "用户id", required = true) @PathVariable("cid") Integer cid,
-			@ApiParam(value = "钓场id", required = true) @PathVariable("uid") Integer uid) {
+			@PathVariable(value = "cid", required = true) int cid
+			, @PathVariable(value="uid", required = true) int uid
+			) {
 		logger.debug("cid={}, uid={}", cid, uid);
 
 		CampaignActor ca = campaignService.getCampaignActor(cid, uid);
@@ -83,12 +84,15 @@ public class CampaignController {
 	}
 
 	@ApiOperation(value = "获取活动参与人（报名）列表", response = List.class) // FIXME
-																	// 这里返回List，而不是List<CampaignActor>，不确定Swagger会怎么处理
+	@ApiImplicitParams({
+		@ApiImplicitParam(paramType = "path", name = "id", value = "活动id", dataType = "int", required = true)
+		, @ApiImplicitParam(paramType = "path", name = "top", value = "取前几位，用于小程序活动首页显示，传-1则不限制", dataType = "int", required = true)
+	})																
 	@RequestMapping(value = "actor/list/{id}/{top}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public List<CampaignActor> getCampaignActorList(
-			@ApiParam(value = "活动id", required = true) @PathVariable("id") Integer id,
-			@ApiParam(value = "取前几位，用于小程序活动首页显示，传-1则不限制", required = true) @PathVariable("top") Integer top) { // FIXME 这里可以改成不传则取全部
+			@PathVariable(value = "id", required = true) int id
+			, @PathVariable(value="top", required = true) int top) { // FIXME 这里可以改成不传则取全部
 		logger.debug("id={}, top={}", id, top);
 
 		List<CampaignActor> campaignActorList = campaignService.getCampaignActorList(id, top);
@@ -96,30 +100,35 @@ public class CampaignController {
 	}
 
 	@ApiOperation(value = "获取活动列表", response = List.class)
+	@ApiImplicitParams({@ApiImplicitParam(paramType = "query", name = "start", value = "起始页", dataType = "int", required = true, example = "1", defaultValue = "1")
+		, @ApiImplicitParam(paramType = "query", name = "size", value = "页大小", dataType = "int", required = true, example = "10")
+	})
 	@RequestMapping(value = "list", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public List<Campaign> getCampaignList(
-			@ApiParam(value = "起始值，首页从1开始", required = true) @RequestParam("start") Integer start,
-			@ApiParam(value = "返回的记录数", required = true) @RequestParam("size") Integer size) {
+			@RequestParam(value = "start", required = true) int start
+			, @RequestParam(value = "size", required = true) int size
+			) {
 		logger.debug("start={}, size={}", start, size);
 
-		if (start == null || size == null) {
-			throw new BadRequestException("缺少必要参数：start/size");
-		}
-		
 		start = start - 1 <= 0 ? 0 : (start - 1) * size;
 		List<Campaign> campaignList = campaignService.getCampaignList(start, size);
 		return campaignList;
 	}
 
 	@ApiOperation(value = "报名--小程序端")
+	@ApiImplicitParams({
+		 @ApiImplicitParam(paramType = "query", name = "uid", value = "用户id", dataType = "long", required = true), 
+		 @ApiImplicitParam(paramType = "query", name = "cid", value = "活动id", dataType = "int", required = true),
+		 @ApiImplicitParam(paramType = "query", name = "openid", value = "Openid", dataType = "String", required = true)
+		})
 	@RequestMapping(value = "actor/miniPay", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
 	// FIXME 思考这里的返回参数，不应当是String的
 	public String miniEnter(
-			@ApiParam(value = "用户id", required = true) @PathVariable("uid") Long uid,
-			@ApiParam(value = "活动id", required = true) @PathVariable("cid") Integer cid,
-			@ApiParam(value = "OpenId", required = true) @PathVariable("openid") String openId) {
+			@RequestParam(value = "uid", required = true) long uid,
+			@RequestParam(value = "cid", required = true) Integer cid,
+			@RequestParam(value = "openid", required = true) String openId) {
 		logger.debug("uid={}, cid={}, openid={}", uid, cid, openId);
 
 		String result = campaignService.enter(uid, cid, openId, 1);
@@ -127,22 +136,28 @@ public class CampaignController {
 	}
 
 	@ApiOperation(value = "报名确认--小程序端")
+	@ApiImplicitParams({
+		 @ApiImplicitParam(paramType = "query", name = "uid", value = "用户id", dataType = "int", required = true), 
+		 @ApiImplicitParam(paramType = "query", name = "cid", value = "活动id", dataType = "int", required = true)
+		})
 	@RequestMapping(value = "actor/miniConfirm", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
 	// FIXME 思考这里的返回参数，不应当是String的
 	public void miniConfirm(
-			@ApiParam(value = "用户id", required = true) @PathVariable("uid") Long uid,
-			@ApiParam(value = "活动id", required = true) @PathVariable("cid") Integer cid) {
+			@RequestParam(value = "uid", required = true) Long uid,
+			@RequestParam(value = "cid", required = true) Integer cid) {
 		logger.debug("uid={}, cid={}", uid, cid);
 
 		campaignService.payActivityConfirm(uid, cid);
 	}
 
+
 	// FIXME 修改url
 	@ApiOperation(value = "回顾详情列表")
+	@ApiImplicitParam(paramType = "path", name = "id", value = "活动id", dataType = "int", required = true, example = "1")
 	@RequestMapping(value = "article/list/{id}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
-	public List<ActivityArticle> getListById(@ApiParam(value = "活动id", required = true) @PathVariable("id") Integer id) {
+	public List<ActivityArticle> getListById(@PathVariable(value = "id", required = true) Integer id) {
 		logger.debug("id={}", id);
 		
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -153,29 +168,14 @@ public class CampaignController {
 
 	// FIXME 修改url
 	@ApiOperation(value = "获取单个回顾详情", response = ActivityArticle.class)
+	@ApiImplicitParam(paramType = "path", name = "id", value = "文章id", dataType = "long", required = true, example = "1")
 	@RequestMapping(value = "article/id/{id}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
-	public ActivityArticle getListById(@ApiParam("文章id") @PathVariable("id") Long id) {
+	public ActivityArticle getListById(@PathVariable(value = "id", required = true) Long id) {
 		logger.debug("id={}", id);
 		
 		ActivityArticle result = activityArticleService.getById(id);
 		return result;
-	}
-	
-	@ApiOperation(value = "微信活动报名(线下)", notes = "参数说明：<br />"
-			+ "cid，活动id<br />"
-			+ "uid，用户id")
-	@ApiImplicitParams({
-		@ApiImplicitParam(paramType = "path", name = "cid", value = "活动id", dataType = "int", required = true)
-		, @ApiImplicitParam(paramType = "path", name = "uid", value = "用户id", dataType = "long", required = true)
-	})
-	@RequestMapping(value = "checkin/{cid}/{uid}", method = RequestMethod.POST)
-	public void checkin(
-			@PathVariable(name = "cid", required = true) int cid
-			, @PathVariable(name = "uid", required = true) long uid) {
-		logger.info("cid:{}, uid:{}", cid, uid);
-		
-		campaignService.enter(uid, cid, null, 0);
 	}
 
 }
